f79f266d0b81925cef13f13b7b970ee1d838860a
wlg223 <wesguarneri@gmail.com>
2024-08-17 13:13:36 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeScreenComponent } from './components/home-screen/home-screen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/home-screen/home-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-screen.component.html',
+  styleUrl: './home-screen.component.scss'
+})
+export class HomeScreenComponent {
+
+}
